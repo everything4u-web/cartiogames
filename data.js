3179c398cd22885e076ad960a81fa01f623c6c00
everything4u-web <everythingforyou1122@gmail.com>
2026-04-02 12:02:02 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,20 @@
+const games = [
+  {
+    name: "Racing Pro",
+    category: "racing",
+    url: "https://html5.gamedistribution.com/rvvASMiM/6c4d5a6/index.html",
+    image: "https://via.placeholder.com/300x150"
+  },
+  {
+    name: "Shooter X",
+    category: "action",
+    url: "https://html5.gamedistribution.com/rvvASMiM/1b2c3d4/index.html",
+    image: "https://via.placeholder.com/300x150"
+  },
+  {
+    name: "Puzzle Master",
+    category: "puzzle",
+    url: "https://html5.gamedistribution.com/rvvASMiM/5e6f7g8/index.html",
+    image: "https://via.placeholder.com/300x150"
+  }
+];
